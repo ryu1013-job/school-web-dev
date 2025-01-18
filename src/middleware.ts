37a97e1ff5fd3b2560086f548ae29e1324199b77
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-01-18 17:35:05 +0900

    👍

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,27 @@
 import { betterFetch } from "@better-fetch/fetch";
-import { type NextRequest, NextResponse } from 'next/server'
-import { auth } from '~/lib/auth';
+import { type NextRequest, NextResponse } from "next/server";
+import { auth } from "~/lib/auth";
 
 type Session = typeof auth.$Infer.Session;
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+  const { pathname } = request.nextUrl;
 
   if (
-    pathname.endsWith('.svg') ||
-    pathname.endsWith('.ico') ||
-    pathname.endsWith('.jpg') ||
-    pathname.endsWith('.jpeg') ||
-    pathname.endsWith('.png') ||
-    pathname.endsWith('.gif')
+    pathname.endsWith(".svg") ||
+    pathname.endsWith(".ico") ||
+    pathname.endsWith(".jpg") ||
+    pathname.endsWith(".jpeg") ||
+    pathname.endsWith(".png") ||
+    pathname.endsWith(".gif")
   ) {
-    return NextResponse.next()
+    return NextResponse.next();
   }
 
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/kouki', request.url))
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/kouki", request.url));
   }
 
-  if (pathname.startsWith('/api')) {
-    return NextResponse.next()
-  }
-
-  const { data: session } = await betterFetch<Session>(
-		"/api/auth/get-session",
-		{
-			baseURL: request.nextUrl.origin,
-			headers: {
-				//get the cookie from the request
-				cookie: request.headers.get("cookie") || "",
-			},
-		},
-  );
-  
   const protectedPaths = [
     "/kouki",
     "/kouki/follow",
@@ -44,26 +29,40 @@ export async function middleware(request: NextRequest) {
     "/kouki/users",
     "/kouki/settings",
   ];
- 
-	if (!session && protectedPaths.includes(pathname)) {
-		return NextResponse.redirect(new URL("/kouki/auth", request.url));
-  }
-  if (session && pathname === "/kouki/auth") {
-    return NextResponse.redirect(new URL("/kouki", request.url));
+
+  if (protectedPaths.includes(pathname)) {
+    const { data: session } = await betterFetch<Session>(
+      "/api/auth/get-session",
+      {
+        baseURL: request.nextUrl.origin,
+        headers: {
+          cookie: request.headers.get("cookie") || "",
+        },
+      }
+    );
+
+    if (!session) {
+      return NextResponse.redirect(new URL("/kouki/auth", request.url));
+    }
+  
+    if (pathname === "/kouki/auth") {
+      return NextResponse.redirect(new URL("/kouki", request.url));
+    }
   }
 
-	return NextResponse.next();
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     {
       source:
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
       missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
       ],
     },
   ],
-}
+};
