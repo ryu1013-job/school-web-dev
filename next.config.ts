7e889220edba41fec9210a08429bc07805cf6b8a
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-01-18 17:03:06 +0900

    👍

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,18 @@ const nextConfig: NextConfig = {
     "@mantine/form",
     "@mantine/dates",
   ],
+  env: {
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_PORT: process.env.DATABASE_PORT,
+    DATABASE_USER: process.env.DATABASE_USER,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 };
 
 export default nextConfig;
